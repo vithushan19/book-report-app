9ecc8b45dd0ed3c25b88c1e191d683bee2f86637
Vithushan Namasivayasivam <vithushan19@users.noreply.github.com>
2023-04-26 11:52:01 -0400

    Add a button and text input to the home page

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default () => (
-    <div>
-        
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Book Report</h1>
+        <p className="lead">
+            Ask any question about this book as if you were talking to a human.          
+        </p>
+        <hr className="my-4" />
+
+        <input className="form-control question-input" type={"text"} />
+        <button
+          className="btn btn-lg custom-button"
+        >
+          Submit
+        </button>
+      </div>
     </div>
+  </div>
 );
