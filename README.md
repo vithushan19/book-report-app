49a94a80277eb28bd05af4f2ebc819ea3cbab7c2
Vithushan Namasivayasivam <vithushan19@users.noreply.github.com>
2023-04-29 20:50:51 -0400

    Fix generate embeddings script

--- README.md
+++ README.md
@@ -1,24 +1,7 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+[x] Pre-process your knowledge base and generate embeddings for each page
+[ ] Store your embeddings to be referenced later
+[x] Build a search page that prompts your user for input
+[ ] Take user's input, generate a one-time embedding, then perform a similarity search against your pre-processed embeddings.
+[ ] Return the most similar pages to the user
---
+++ lib/generateEmbeddings.rb
@@ -0,0 +1,43 @@
+require 'openai'
+require 'json'
+
+# Set up the OpenAI API key
+
+client = OpenAI::Client.new(access_token: api_key)
+
+# Read the contents of the input file
+input_file_path = 'book-input.txt'
+input_text = File.read(input_file_path)
+
+chunk_size = 8191
+overlap = 20
+
+# Split text into chunks
+chunks = []
+start = 0
+while start < input_text.length
+  chunk = input_text[start, chunk_size]
+  chunks << chunk
+  start += chunk_size - overlap
+end
+
+# Fetch embeddings for each chunk
+embeddings = chunks[0,1].map { |chunk| client.embeddings(
+    parameters: {
+        model: "text-embedding-ada-002",
+        input: chunk
+    }
+) }
+
+# Write embeddings to file
+File.open('output.txt', 'w') do |file|
+  result = []
+  embeddings.each_with_index do |embedding, index|
+    result << { "content": "#{chunks[index]}", "embedding": embedding["data"][0]["embedding"]}
+  end
+  
+  file.write(JSON.pretty_generate(result))
+end
+
+
+puts "Embedding saved to output.txt"
