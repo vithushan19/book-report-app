b280136b673635f9da31cdf2285971e6df3d8e16
Vithushan Namasivayasivam <vithushan19@users.noreply.github.com>
2023-05-02 14:44:03 -0400

    Clean question string
    Clean question string before querying or inserting into the database

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -6,71 +6,7 @@ class HomepageController < ApplicationController
   def index
   end
 
-  def cosine_similarity(array1, array2)
-    # Check if the arrays have the same length
-    if array1.length != array2.length
-      raise ArgumentError, "The arrays must have the same length"
-    end
-  
-    # Calculate the dot product and magnitudes
-    dot_product = 0.0
-    magnitude1 = 0.0
-    magnitude2 = 0.0
-    array1.each_with_index do |value1, index|
-      value2 = array2[index]
-      dot_product += value1 * value2
-      magnitude1 += value1 ** 2
-      magnitude2 += value2 ** 2
-    end
-  
-    # Calculate the cosine similarity
-    magnitude1 = Math.sqrt(magnitude1)
-    magnitude2 = Math.sqrt(magnitude2)
-    cosine_similarity = dot_product / (magnitude1 * magnitude2)
-  
-    cosine_similarity
-  end
-
-  def getAnswerFromOpenAI(title, question)
-    question_embedding = OpenaiService.fetch_embedding_for_question(question)[:embedding] # Fetch embeddings using OpenAI service
-    book_sections = BookSection.select('*')
-
-    # Calculate the cosine similarity between the query and each book section
-    book_sections_with_similarities = book_sections.map { |book_section|
-      book_section_hash = book_section.attributes
-
-      similarity = cosine_similarity(book_section.embeddings, question_embedding)
-      book_section_hash["similarity"] = similarity
-      book_section_hash
-    }
-
-    # Sort the array of hashes by the 'value' attribute in descending order
-    sections_sorted_by_semantic_relevance = book_sections_with_similarities.sort_by { |item| item["content"] }.reverse
-
-    # Create a prompt using the top 2 most semantically relevant sections
-    prompt = "Here are relvant sections from a book called #{title} 
-    
-    #{sections_sorted_by_semantic_relevance[0]["content"]} 
-    #{sections_sorted_by_semantic_relevance[1]["content"]} 
-    
-    Here is a question about the book #{title}. 
-    
-    Question: #{question} 
-    
-    Provide a detailed answer to the question. 
-    
-    If you do not know the answer then says 'I don't know the answer given the information from the book'"
-
-    answer = OpenaiService.fetch_completion_for_prompt(prompt) # Fetch completion using OpenAI service
-
-    puts "prompt"
-    puts prompt
-    puts "answer"
-    puts answer
-    return answer
-  end
-
-  def getRecentQuestions
+  def get_recent_questions
     book_title = params[:book]
 
     # Get the most recent 10 questions for the specified book
@@ -85,7 +21,7 @@ class HomepageController < ApplicationController
     book_title = params[:book]
     
     # Use the find_by method to search for the question-answer pair by question
-    book_question = BookQuestion.find_by(question: question)
+    book_question = BookQuestion.find_by(question: question.downcase.strip)
 
     # Check if a matching pair was found
     if book_question
@@ -104,7 +40,7 @@ class HomepageController < ApplicationController
       if answer == nil || answer == "" || answer == " " 
         puts "No answer found in OpenAI for the specified question. Try asking a different question."
       else
-        BookQuestion.create(book_title: book_title, question: question, answer: answer)
+        BookQuestion.create!(book_title: book_title, question: question.downcase.strip, answer: answer)
       end
     end
     
